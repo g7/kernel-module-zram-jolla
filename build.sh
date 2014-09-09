8557ec277a2a38fa4934f60aa64a25584f3d730d
Eugenio Paolantonio (g7) <me@medesimo.eu>
2014-09-09 11:05:13 +0200

    build.sh: ensure we create the needed directories.

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 echo "RPMBUILD SUCKS"
+for directory in "SRPMS" "RPMS" "BUILDROOT" "BUILD"; do
+	[ ! -e $directory ] && mkdir $directory
+done
 exec rpmbuild --define "_topdir $PWD" -ba kernel-module-zram.spec
